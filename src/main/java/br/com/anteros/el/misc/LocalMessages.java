ef4d0951b7911c304359c1d607f41cec6ed5a367
Edson Martins <edsonmartins2005@gmail.com>
2014-10-12 11:24:42 -0300

    Correção na tradução e licença.

--- src/main/java/br/com/anteros/el/misc/LocalMessages.java
+++ src/main/java/br/com/anteros/el/misc/LocalMessages.java
@@ -1,18 +1,18 @@
-/*
- * Copyright 2006-2009 Odysseus Software GmbH
- *
+/*******************************************************************************
+ * Copyright 2012 Anteros Tecnologia
+ *  
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *******************************************************************************/
 package br.com.anteros.el.misc;
 
 import java.text.MessageFormat;
@@ -20,7 +20,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public final class LocalMessages {
-	private static final String BUNDLE_NAME = "de.odysseus.el.misc.LocalStrings";
+	private static final String BUNDLE_NAME = "META-INF/anterosel_messages.properties";
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
 	public static String get(String key, Object... args) {
